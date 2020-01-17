728c3345c133a07edbcb7a2b77a16980df564e66
thetiger756 <46494603+thetiger756@users.noreply.github.com>
2020-01-17 13:55:14 -0500

    PlayerController v1.2
    This should be done or close to done.
    + Fixed acceleration
    + Other Improvements
    + More comments
    - Vertical movement still takes priority

--- PlayerController.cs
+++ PlayerController.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
-    //Additional component that should be used with this script
+    //By Seth Ruiz
+    //I am still learning how to code, so this will be sloppy.
+    //But at least there are comments.
+
+    //Bring in the controller component
     public CharacterController controller;
 
-    //Speed control
+    //How fast do we want to go
     private float playerSpeed = 3;
 
-    //TODO: Make speed a set value, rather than having the player accelerate
+    //To eliminate acceleration
+    //We need to keep track of what the player was inputting last frame
+    private float xLog = 0;
+    private float zLog = 0;
+    //And interpret the correct int to round to based on that and their current input
+    private float xRounded = 0;
+    private float zRounded = 0;
 
     void Start()
     {
@@ -20,27 +29,90 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        //look for input every frame
         playerEngine();
     }
 
     void playerEngine()
     {
-        //Look for WASD and Arrow Key Input
+        //Look for WASD and Arrow Key Input. 
+        //x and z will always be some decimal between -1 and 1
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
+        //To get rid of acceleration we want to round to an int.
+        //We have to use multiple rules so that we can interpret
+        //player intent and keep the game feeling responsive
+
+        //When moving right and speeding up
+        if (x > 0 && x > xLog)
+        {
+            //Round up
+            xRounded = Mathf.CeilToInt(x);
+        }
+        //Make the player stop immediately when they let go
+        else if(x > 0 && x < xLog)
+        {
+            xRounded = 0;
+        }
+
+        //Now for the left
+        else if (x < 0 && x < xLog)
+        {
+            xRounded = Mathf.FloorToInt(x);
+        }
+        else if (x < 0 && x > xLog)
+        {
+            xRounded = 0;
+        }
+
+        //And this keeps edge cases snappy
+        else if (x == 0)
+        {
+            xRounded = 0;
+        }
+
+        //repeat above for vertical movement
+        if (z > 0 && z > zLog)
+        {
+            zRounded = Mathf.CeilToInt(z);
+        }
+        else if (z > 0 && z < zLog)
+        {
+            zRounded = 0;
+        }
+        else if (z < 0 && z < zLog)
+        {
+            zRounded = Mathf.FloorToInt(z);
+        }
+        else if (z < 0 && z > zLog)
+        {
+            zRounded = 0;
+        }
+        else if (z == 0)
+        {
+            zRounded = 0;
+        }
+
         //Limit movement to one axis at a time
-        if(z != 0)
+        //Unfortunately this creates a situation where vertical input takes
+        //priority over horizontal movement. Maybe this can be fixed in the future.
+        while (z != 0)
         {
             x = 0;
         }
 
-        //Apply Movements
-        Vector3 move = transform.right * x + transform.forward * z;
+        //Create a vector3 (0, 0, 0) called "move"
+        //transform.right is the first part of the vector3; it is modified by the player's input
+        //transform.forward is the last part of the vector3; it is also modified by the player's input
+        Vector3 move = transform.right * xRounded + transform.forward * zRounded;
 
-        //Take set speed into account
+        //Tell the controller to move the player based on their input modified by our set speed
         controller.Move(move * playerSpeed * Time.deltaTime);
 
+        //write down what x and z were for this frame
+        xLog = x;
+        zLog = z;
+
+        Debug.Log(xRounded);
     }
 }
